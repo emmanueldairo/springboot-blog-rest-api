0ba118324d996836bafef69bac118eee89ef729c
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-08-03 12:37:04 -0400

    -added Sorting feature

--- src/main/java/com/Springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/Springboot/blog/service/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import com.Springboot.blog.service.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -35,8 +36,8 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNo,pageSize);
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNo,pageSize, Sort.by(sortBy));
 
         Page<Post> posts = postRepository.findAll(pageable);
         //get content for page object
