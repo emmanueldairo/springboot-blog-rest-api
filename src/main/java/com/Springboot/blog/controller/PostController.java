9689d383fa252f64094c316f5ad4eb601f85627b
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-08-03 10:13:49 -0400

    -added GetAllPosts & GetPostsById

--- src/main/java/com/Springboot/blog/controller/PostController.java
+++ src/main/java/com/Springboot/blog/controller/PostController.java
@@ -4,10 +4,9 @@ import com.Springboot.blog.payload.PostDTO;
 import com.Springboot.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -17,8 +16,23 @@ public class PostController {
     public PostController(PostService postService) {
         this.postService = postService;
     }
+
+    //create blog rest api
     @PostMapping
     public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO){
         return new ResponseEntity<PostDTO>(postService.createPost(postDTO), HttpStatus.OK);
+    }
+
+
+    //get all posts rest api
+    @GetMapping
+    public List<PostDTO> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    //get post by id
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable("id") long id){
+        return  ResponseEntity.ok(postService.getPostById(id));
     }
 }
