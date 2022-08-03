b4910d45f4e282efe62572701f8fa0ad844e713a
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-08-02 20:28:25 -0400

    added post controller

---
+++ src/main/java/com/Springboot/blog/conroller/PostController.java
@@ -0,0 +1,24 @@
+package com.Springboot.blog.conroller;
+
+import com.Springboot.blog.payload.PostDTO;
+import com.Springboot.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO){
+        return new ResponseEntity<PostDTO>(postService.createPost(postDTO), HttpStatus.OK);
+    }
+}
