f8d0a1b90241b9393f90f3c1ad2ac60e304f7fef
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-08-03 12:51:02 -0400

    -created app constants

---
+++ src/main/java/com/Springboot/blog/utils/AppConstants.java
@@ -0,0 +1,8 @@
+package com.Springboot.blog.utils;
+
+public class AppConstants {
+    public static final String DEAFAULT_PAGE_NUMBER = "0";
+    public static final String DEAFAULT_PAGE_SIZE = "10";
+    public static final String DEAFAULT_SORT_BY = "id";
+    public static final String DEAFAULT_SORT_DIRECTION = "asc";
+}
