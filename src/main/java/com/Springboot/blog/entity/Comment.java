10714e93e106bd7f539f1ae99184d9f5c3d37569
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-08-04 09:13:48 -0400

    -created the relationship between comment and post

--- src/main/java/com/Springboot/blog/entity/Comment.java
+++ src/main/java/com/Springboot/blog/entity/Comment.java
@@ -1,5 +1,25 @@
 package com.Springboot.blog.entity;
 
-public class Comment {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String body;
+    //establishing the relationship between posts and comments
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 }
