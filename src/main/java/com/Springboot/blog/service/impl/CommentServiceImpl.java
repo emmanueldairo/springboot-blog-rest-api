8bf20992e88f0489e9c881b34be0079a9414092b
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-08-04 23:15:23 -0400

    -added update comment  feature

--- src/main/java/com/Springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/Springboot/blog/service/impl/CommentServiceImpl.java
@@ -61,6 +61,21 @@ public class CommentServiceImpl implements CommentService {
         return mapToDto(comment);
     }
 
+    @Override
+    public CommentDto updateComment(long postId, long commentId, CommentDto commentRequest) {
+        Post post = postRepository.findById(postId).orElseThrow(()-> new ResourceNotFoundException("Post","id",postId));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(()->new ResourceNotFoundException("Comment","id",commentId));
+
+        if (!(comment.getPost().getId() == post.getId())){
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Comment does not belong to the post");
+        }
+        comment.setName(commentRequest.getName());
+        comment.setEmail(commentRequest.getEmail());
+        comment.setBody(commentRequest.getBody());
+        Comment updatedComment = commentRepository.save(comment);
+        return mapToDto(updatedComment);
+    }
+
     private CommentDto mapToDto(Comment comment){
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
